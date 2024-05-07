b8985d632cca908e758d3742a8101d91a0bbd43f
Kenneth Munk <da.munkk@gmail.com>
2024-05-07 11:58:00 -0700

    Building out the scripts while implementing an instance id match

---
+++ ComplianceCheck.ps1
@@ -0,0 +1,37 @@
+$RawDeviceList = Get-PnpDevice | 
+    where {$_.FriendlyName -Match "(Wi-Fi)|(Ethernet)|(Hub)"} | 
+    where {$_.FriendlyName -notmatch "Virtual"} | 
+    where {$_.Class -notmatch "Software"};
+
+$PowerControlledDevices = Get-CimInstance -ClassName MSPower_DeviceEnable -namespace root\wmi;
+
+$TargetDevices = @();
+
+#Write-Host "Raw devices";
+
+#Checking what the outputs are
+foreach($rawdevice in $RawDeviceList){
+    $rawInstanceID =  $rawdevice.InstanceID.Trim();
+    #Write-Host $rawInstanceID;
+    $rawInstanceID = $rawInstanceID.replace("\","\\");
+    foreach($PowerDevice in $PowerControlledDevices){
+        if($PowerDevice.InstanceName -match $rawInstanceID){
+            #Write-Host $PowerDevice;
+            $TargetDevices += $PowerDevice;
+        }
+    }
+}
+
+#Write-Host "Target Devices"
+
+$InCompliance = $true;
+foreach($Target in $TargetDevices){
+    #Write-Host $Target;
+    #Write-Host "Enable Status:"$Target.Enable;
+    if($Target.Enable -eq "True"){
+        #Write-Host "Enable Status:"$Target.Enable "for" $Target;
+        $InCompliance = $false;
+    }
+}
+
+Write-Output $InCompliance;
